89192a48d783c61775414f59e5a6f5e5f6d31e1f
Mark Reid <mreid@mozilla.com>
2016-03-16 09:30:55 -0300

    Insert the "spark" prefix into SparkUI responses.
    Hackishly insert the spark link prefix ("/spark/") into docs
    returned from the Spark UI. This makes the resulting page work
    as expected, including links, images, and css/js.
    
    It may be too fragile, so if there is a better way to do this, I
    am open to changing the approach.

--- jupyter-spark/spark.py
+++ jupyter-spark/spark.py
@@ -5,6 +5,7 @@ import tornado
 import os
 import logging
 import json
+from BeautifulSoup import BeautifulSoup
 
 EXTENSION_URL = "/spark"
 
@@ -18,17 +19,45 @@ def raise_error(msg):
 
 class SparkHandler(IPythonHandler):
     spark_host = None
+    web_app = None
 
     def get(self):
         #tornado_logger.info("Received URI from client: " + self.request.uri)
         if not self.request.uri.startswith(EXTENSION_URL):
             raise_error("URI did not start with " + EXTENSION_URL)
-        spark_request = spark_host + self.request.uri[len(EXTENSION_URL):]
+        spark_request = self.spark_host + self.request.uri[len(EXTENSION_URL):]
         #tornado_logger.info("Sending request to Spark UI: " + spark_request)
 
         try:
             spark_response = requests.get(spark_request)
-            client_response = spark_response.text
+
+            content_type = spark_response.headers['content-type']
+            self.set_header("Content-Type", content_type)
+
+            if "text" in content_type:
+                # Replace all the relative links with our proxy crap
+                soup = BeautifulSoup(spark_response.text)
+
+                for has_href in ['a', 'link']:
+                    for a in soup.findAll(has_href):
+                        try:
+                            a['href'] = url_path_join(self.web_app, a['href'])
+                        except KeyError:
+                            print "boo", a
+
+                for has_src in ['img', 'script']:
+                    for a in soup.findAll(has_src):
+                        try:
+                            a['src'] = url_path_join(self.web_app, a['src'])
+                        except KeyError:
+                            print "boo", a
+
+                client_response = str(soup)
+            else:
+                # Probably binary response, send it directly.
+                client_response = spark_response.content
+
+
             #tornado_logger.info("Receiving response from Spark UI: " + client_response)
         except requests.exceptions.RequestException:
             client_response = json.dumps({"error": "SPARK_NOT_RUNNING"})
@@ -47,3 +76,5 @@ def load_jupyter_server_extension(nb_server_app):
     host_pattern = ".*$"
     route_pattern = url_path_join(web_app.settings['base_url'], EXTENSION_URL) + ".*"
     web_app.add_handlers(host_pattern, [(route_pattern, SparkHandler)])
+
+    SparkHandler.web_app = url_path_join(web_app.settings['base_url'], EXTENSION_URL)
